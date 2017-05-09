f3f5df343b753de6637bc62e30c3d044469096ef
Chang liu <eeliuchang@gmail.com>
2017-05-09 14:13:39 -0500

    Add files via upload

--- distance.py
+++ distance.py
@@ -37,13 +37,16 @@ def pearson_correlation(v1, v2):
 
 
 def cosine_correlation(v1, v2):
-	v1_std = math.sqrt(sum([(v1[i]) ** 2 for i in range(len(v1))])*1.0/len(v1))
-	v2_std = math.sqrt(sum([(v2[i]) ** 2 for i in range(len(v2))])*1.0/len(v2))
-	cov = sum([(v1[i]) * (v2[i]) for i in range(len(v1))])*1.0/len(v1)
-	return cov*1.0/v1_std/v2_std 
+	if (len(v1) == 0):
+		return 0
+	else:
+		v1_std = math.sqrt(sum([(v1[i]) ** 2 for i in range(len(v1))])*1.0/len(v1))
+		v2_std = math.sqrt(sum([(v2[i]) ** 2 for i in range(len(v2))])*1.0/len(v2))
+		cov = sum([(v1[i]) * (v2[i]) for i in range(len(v1))])*1.0/len(v1)
+		return cov*1.0/v1_std/v2_std 
 
 
 
-pref = {1:{'python':1, 'java':2, 'c':3}, 2: {'python':3, 'scala':2, 'java':1}, 3: {'python':2, 'c++': 1, 'java': 2}}
-sol1 = distance(pref, 1,3, pearson_correlation)
-print(sol1)
+#pref = {1:{'python':1, 'java':2, 'c':3}, 2: {'python':3, 'scala':2, 'java':1}, 3: {'python':2, 'c++': 1, 'java': 2}}
+#sol1 = distance(pref, 1,3, pearson_correlation)
+#print(sol1)
---
+++ recommender_system.py
@@ -0,0 +1,25 @@
+from input_file import input_data, item_name_map, match_item
+from recommend import recommend
+from distance import cosine_correlation, pearson_correlation
+
+def recommender_system(path_to_data, path_to_item, person, distance_metric):
+	""" 
+	This is the main file to combine all the other functions in order to recommend 
+	products to a user.
+	"""
+	data = input_data(path_to_data)
+	item_name = item_name_map(path_to_item)
+	recommend_items = recommend(data, person, distance_metric)
+	result = match_item(recommend_items, item_name)
+	return result
+
+
+
+path_to_data = "./ml-100k/u.data"
+path_to_item = "./ml-100k/u.item"
+result = recommender_system(path_to_data, path_to_item, '934', cosine_correlation)
+for elem in result[:10]:
+	print(elem)
+
+
+
